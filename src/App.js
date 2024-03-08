5a13ceb5606e20c5c7476f43f74ac4969dc27387
pompokorin <147459878+pompokorin@users.noreply.github.com>
2024-03-09 10:58:16 +1100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/pompokorin">Mina Miyamoto</a> and is{" "}
+        <a
+          href="https://github.com/pompokorin/reaact-weather-app"
+          target="blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
