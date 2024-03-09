385fc686ddaef15d628ef071d095d600c835c12d
pompokorin <147459878+pompokorin@users.noreply.github.com>
2024-03-09 22:26:55 +1100

    weather icon updates to animated icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,7 +14,7 @@ export default function WeatherInfo(props) {
             <i class="fa-solid fa-location-dot place"></i> {props.data.city}
           </li>
           <li>
-            <img src={props.data.icon}></img>
+            <WeatherIcon code={props.data.icon} />
             <span id="current-temp">
               {Math.round(props.data.temperature)}°C
             </span>
